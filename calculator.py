769e3c2bde68cea1a5f5e3b0da6179799f437801
learn typescript  with sr <syedarohab8@gmail.com>
2026-09-02 23:55:08 +0500

    Add functions to safely fetch list item and add numbers

---
+++ calculator.py
@@ -0,0 +1,10 @@
+def get_item(lst, index):
+    """Safely fetch an item from a list, returning None if out of range."""
+    if lst is None:
+        return None
+    if index < 0 or index >= len(lst):
+        return None
+    return lst[index]
+
+def add(a, b):
+    return a + b
